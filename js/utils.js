f1d8e69a302de08249e4e5b4254a05c8e4eacb13
DiasSad <dias.sadybekov@mail.ru>
2024-03-14 14:57:57 +0600

    Сделал второй лвл, третий не смог :(

--- js/utils.js
+++ js/utils.js
@@ -38,13 +38,74 @@ const heroAttack = function () {
 }
 const dragonAttack = function () {
     if (getRandomInt(50)) {
-        Hero.modifyHealth();
         Hero.showInfo();
+        Hero.modifyHealth();
         // console.log(`Дракон нанес ${dragonDamage} урона герою, у героя осталось ${Hero.hp}`);
         if (getRandomInt(50)) {
             console.log(`Дракон решил поспать`);  
         }
     }
-    } 
+} 
 
+const heroQuestion = prompt('Что делать герою? Выбери действие: 1 - Атаковать, 2 - ничего не делать, 3 - одеть щит, 4 - снять щит');
+const varHero = function () {
+    switch (parseInt(heroQuestion)) {
+        case 1:
+            heroAttack();
+            console.log('Герой атакует!');
+            break;
+        case 2:
+            console.log('Героя ничего не делает');
+            break;
+        case 3:
+            Hero.equipShield();
+            console.log('Герой защищается!');
+            break;
+        case 4: 
+            Hero.removeShield(); 
+        default:
+            console.log('Нету такого выбора');
+            break;
+    }
+}
 
+// const nothingToDo = function () {
+    
+// }
+
+// const toDefend = function () {
+//     if (Hero.equipShield -= Dragon.dragonAttack) {
+//         console.log('Вы одели щит');
+//         if (Hero.removeShield -= dragonAttack) {
+//             console.log('Вы сняли щит');
+//         }
+//     } 
+// }
+
+const equipShield = function () {
+    if (this.isShieldEquipped) {
+        this.defense += this.shield;
+        this.isShieldEquipped = true;
+        console.log('Вы экипировали щит');
+    } else {
+        console.log('Щит уже экипирован');
+    }
+}
+const removeShield = function () {
+    if (this.isShieldEquipped) {
+        if (this.defense > this.shield) {
+            this.defense -= this.shield;
+            this.isShieldEquipped = false;
+            console.log('Вы сняли щит.');
+        } else {
+            console.log('Ваша защита уменьшилась.');
+        }
+    } else {
+        console.log('Щит не был экипирован');
+    }
+}
+
+// У героя будет три действие, атаковать, ничего не делать, защищаться, при этом нужна возможность выбирать между действиями.
+// Щит должен плюсовать к защите. Щит должен дейстовать только один ход и он не должен накладывать друг на друга.Это означает каждый циикл должен обнуляться.
+// у героя будет два метода, одевать щит и убирать щит. нужно будет сделать проверку, что щит был экипирован раньше, что бы не добавлять вторую порцию, и не убавлять защиту меньше чем базовая.
+// нужно ввести булевое значение isShieldEquipped, которое должно ставить и убираться в этом методе при успешной экипировке и успешном снятии. при попытке снять не одетый щит или одеть щит, который уже был одет, выводить в консоль сообщение.Удачи Диас
